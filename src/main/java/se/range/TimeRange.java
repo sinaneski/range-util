01f0bfdf5f2c1bd0811d33841e46c19e9f5c7648
sinaneski <sinaneski@gmail.com>
2017-11-06 17:15:42 +0000

    Add intersect operation to TimeRange and TimeRangeContainer

--- src/main/java/se/range/TimeRange.java
+++ src/main/java/se/range/TimeRange.java
@@ -15,4 +15,13 @@ public class TimeRange extends Range<Long> {
         if(isEmpty()) return 0;
         return end - start;
     }
+
+    public TimeRange intersect(TimeRange other) {
+        if(!isIntersect(other)) return null;
+        Long s = start, e = end;
+        if(CompareUtil.greaterThan(other.start, start)) s = other.start;
+        if(CompareUtil.lessThan(other.end, end)) e = other.end;
+        return new TimeRange(s, e);
+    }
+
 }
--- src/main/java/se/range/TimeRangeContainer.java
+++ src/main/java/se/range/TimeRangeContainer.java
@@ -14,4 +14,46 @@ public class TimeRangeContainer extends RangeContainer<Long, TimeRange> {
         }
         return totalDiff;
     }
+
+    public  TimeRangeContainer intersect(TimeRangeContainer secondRangeList) {
+         TimeRangeContainer results = new TimeRangeContainer();
+        int j = 0;
+        int i = 0;
+
+        while( i < rangeList.size() && j < secondRangeList.size()) {
+            TimeRange current = rangeList.get(i);
+            TimeRange range = secondRangeList.get(j);
+            if (range.isBefore(current)) {
+                j++;
+                continue;
+            }
+            if (range.isAfter(current)) {
+                i++;
+                continue;
+            }
+            TimeRange result = current.intersect(range);
+
+            if (!result.isEmpty()) {
+                results.add(result);
+            }
+
+            if (CompareUtil.greaterThan(current.getEnd(), result.getEnd())) {
+                j++;
+                continue;
+            }
+            i++;
+        }
+
+        return results;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for(TimeRange t: rangeList) {
+            sb.append(t.toString());
+        }
+        return sb.toString();
+    }
+
 }
